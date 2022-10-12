7eb92c78429bf7554d7c8858285ba1b9da19bd98
crspradlin <github@crspradlin.org>
2022-10-12 12:00:00 +0000

    Added blinking cursor console look

---
+++ components/console.vue
@@ -0,0 +1,12 @@
+<script setup>
+const awaitingInput = ref(true);
+const input = ref("");
+</script>
+
+<template>
+  <div class="text-stone-500 break-all">
+    <div id="history"></div>
+    <span id="terminal-line">$server@crspradlin: {{input}}</span><Cursor v-if="awaitingInput" /><br/>
+    <span id="output"></span>
+  </div>
+</template>
---
+++ components/cursor.vue
@@ -0,0 +1,14 @@
+<script setup>
+const content = ref("");
+setInterval(() => {
+    if (content.value.length) {
+        content.value = "";
+    } else {
+        content.value = "|";
+    }
+}, 500);
+</script>
+
+<template>
+    <span>{{content}}</span>
+</template>
