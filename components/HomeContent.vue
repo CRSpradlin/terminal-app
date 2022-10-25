982063069ae7b7a1a1bbc50493acb3480ad59a67
crspradlin <github@crspradlin.org>
2022-10-24 23:01:51 -0400

    Added basic grow comp for profile content

---
+++ components/HomeContent.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col content-center">
+    <div class="flex flex-row m-auto">
+      <div class="flex flex-col m-auto">
+        <h1>Christopher Spradlin</h1>
+        <span class="flex flex-row m-auto">
+          <button class="m-2" href="https://github.com/crspradlin/">GitHub</button>
+          <button class="m-2" href="https://www.linkedin.com/in/crspradlin/">LinkedIn</button>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
