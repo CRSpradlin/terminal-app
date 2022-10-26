0df300009c0a5d62a60f4d38ffa235d4ec3ab0e9
crspradlin <github@crspradlin.org>
2022-10-25 21:50:03 -0400

    Added styling for buttons and name within HomeContent

--- components/HomeContent.vue
+++ components/HomeContent.vue
@@ -6,10 +6,22 @@
   <div class="flex flex-col content-center">
     <div class="flex flex-row m-auto">
       <div class="flex flex-col m-auto">
-        <h1>Christopher Spradlin</h1>
         <span class="flex flex-row m-auto">
-          <button class="m-2" href="https://github.com/crspradlin/">GitHub</button>
-          <button class="m-2" href="https://www.linkedin.com/in/crspradlin/">LinkedIn</button>
+          <span class="text-7xl p-8">Christopher Spradlin</span>
+        </span>
+        <span class="flex flex-row m-auto">
+          <a class="m-2 p-2 border-2 rounded-full border-slate-50 text-slate-50 hover:bg-slate-50 hover:text-slate-800" href="https://github.com/crspradlin/" target="_blank">
+            <Icon name="bxl:github" />
+            GitHub
+          </a>
+          <a class="m-2 p-2 border-2 rounded-full border-sky-500 text-sky-500 hover:bg-sky-500 hover:text-sky-900" href="https://www.linkedin.com/in/crspradlin/" target="_blank">
+            <Icon name="bxl:linkedin-square" />
+            LinkedIn
+          </a>
+          <a class="m-2 p-2 border-2 rounded-full border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-purple-900" href="https://dev.to/crspradlin" target="_blank">
+            <Icon name="bxl:dev-to" />
+            Dev.to
+          </a>
         </span>
       </div>
     </div>
