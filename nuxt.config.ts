7e4605f03187727de4a2d67fb17e2f07ffab9bc0
crspradlin <github@crspradlin.org>
2022-10-29 00:17:27 -0400

    Added public meta information

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-16',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'CRSpradlin Terminal',
+            meta: [
+                // <meta name="description" content="My amazing site">
+                { name: 'description', content: 'Software Developer, Christopher Spradlin\'s Interactive Personal Site' }
+            ]
+        }
+    },
     modules: ['nuxt-icon'],
     ssr: false,
     build: {
