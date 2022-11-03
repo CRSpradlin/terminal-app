65a913e71085a1b214a97e6aaa8d56e8fd10b382
crspradlin <github@crspradlin.org>
2022-11-02 23:02:03 -0400

    Added animated bg for matrix look

--- app.vue
+++ app.vue
@@ -5,7 +5,8 @@ const config = useAppConfig();
 
 <template>
   <title>{{ config.title }}</title>
-  <div class="mt-[-80px] sm:mt-0 w-full h-full min-w-[300px]">
+  <div class="hidden md:block fixed h-full w-full opacity-[.13] blur-md z-10 bg-[url('/digitalRain.gif')] bg-no-repeat bg-cover bg-center" />
+  <div class="z-0 mt-[-80px] sm:mt-0 w-full h-full min-w-[300px]">
     <ConsoleComponent />
   </div>
 </template>
