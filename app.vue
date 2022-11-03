5959f0f5f1f7087a0d8697660f4740d86d0ad419
crspradlin <github@crspradlin.org>
2022-11-02 23:24:56 -0400

    Fixed button bug related to animated bg image

--- app.vue
+++ app.vue
@@ -5,8 +5,8 @@ const config = useAppConfig();
 
 <template>
   <title>{{ config.title }}</title>
-  <div class="hidden md:block fixed h-full w-full opacity-[.13] blur-md z-10 bg-[url('/digitalRain.gif')] bg-no-repeat bg-cover bg-center" />
-  <div class="z-0 mt-[-80px] sm:mt-0 w-full h-full min-w-[300px]">
+  <div class="hidden md:block fixed h-full w-full opacity-[.13] blur-md bg-[url('/digitalRain.gif')] bg-no-repeat bg-cover bg-center" />
+  <div class="relative bg-transparent mt-[-80px] sm:mt-0 w-full h-full min-w-[300px]">
     <ConsoleComponent />
   </div>
 </template>
